e2b28a9ab18987e19d5688d615b760949d55385f
niihash <50837663+niihash@users.noreply.github.com>
2025-06-07 21:30:03 -0300

    fix(cadastro): show feedback messages

--- resources/views/livewire/register-form.blade.php
+++ resources/views/livewire/register-form.blade.php
@@ -2,20 +2,20 @@
     <div class="form-group animate__animated animate__fadeInUp animate__delay-2s">
         <label class="form-label" for="firstName">Nome</label>
         <input wire:model.defer="name" type="text" id="firstName" name="firstName" class="form-input" placeholder="Seu nome" required>
-        @error('name') <span class="error-message">{{ $message }}</span> @enderror
+        @error('name') <span class="error">{{ $message }}</span> @enderror
     </div>
 
     <div class="form-group animate__animated animate__fadeInUp animate__delay-3s">
         <label class="form-label" for="email">E-mail</label>
         <input wire:model.defer="email" type="email" id="email" name="email" class="form-input" placeholder="seu@email.com" required>
-        @error('email') <span class="error-message">{{ $message }}</span> @enderror
+        @error('email') <span class="error">{{ $message }}</span> @enderror
     </div>
 
     <div class="form-row">
         <div class="form-group animate__animated animate__fadeInLeft animate__delay-4s">
             <label class="form-label" for="password">Senha</label>
             <input wire:model.defer="password" type="password" id="password" name="password" class="form-input" placeholder="••••••••" required>
-            @error('password') <span class="error-message">{{ $message }}</span> @enderror
+            @error('password') <span class="error">{{ $message }}</span> @enderror
         </div>
         <div class="form-group animate__animated animate__fadeInRight animate__delay-4s">
             <label class="form-label" for="confirmPassword">Confirmar Senha</label>
